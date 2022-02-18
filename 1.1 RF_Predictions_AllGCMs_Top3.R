02ed3dd7c3b307054f0959e2711d212490a6348c
Shyam Thomas <thomas.shyam@gmail.com>
2022-02-18 16:54:27 -0600

    Rscript modified and new figure added

--- 1.1 RF_Predictions_AllGCMs_Top3.R
+++ 1.1 RF_Predictions_AllGCMs_Top3.R
@@ -1,14 +1,20 @@
 library(tidyverse)
 library(miceadds)
+library(ggplot2)
+library(randomForest)
+library(reshape2)
 
+### List all RF models
 RFmodels = list.files(path ="processed_data/TrainData/", pattern = "*Top3_.Rdata$")
 RFmodels
 
+### List all test data files (i.e future water temperature)
 TestDataFiles = list.files(path ="processed_data/TestData/ForecastData", pattern = "*.csv$")
 TestDataFiles
 
 new.data=list()
 
+### Loop all test data files to make a new data file with just the three predictors
 for (TestData in TestDataFiles){
   Test = read.csv(paste("processed_data/TestData/ForecastData/",TestData, sep=""))
   Test.preds=Test[,c(5,11,12)]
@@ -23,9 +29,12 @@ for (i in 1:5){
   fut.predictions[[i]]=predict(train.model, newdata = new.data[[i]])
 }
 
-fut.predictions
+head(fut.predictions)
 fut.preds.df=as.data.frame(do.call(cbind,fut.predictions))
+head(fut.preds.df)
 
+
+#### Repeat the above codes with Current temperature(i.e. the training data)
 TrainDataFiles=list.files(path ="processed_data/TrainData", pattern = "*.csv$")
 TrainDataFiles
 
@@ -45,6 +54,7 @@ for (i in 1:5){
 }
 head(curr.predictions)
 curr.preds.df=as.data.frame(do.call(cbind,curr.predictions))
+head(curr.preds.df)
 
 ModelNames=c("ACCESS","GFDL","IPSL","MIROC5","MRI")
 ModelNames
@@ -59,5 +69,5 @@ currANDfut_preds=bind_rows(curr.preds.df,fut.preds.df)
 currANDfut_preds.melt=melt(currANDfut_preds)
 head(currANDfut_preds.melt)
 
-ggplot(currANDfut_preds.melt, aes(x=variable, y=value, fill=Period))+geom_boxplot()+ylab("Invasion Risk")+xlab("GCMs")
-
+InvasionRisk_Plot=ggplot(currANDfut_preds.melt, aes(x=variable, y=value, fill=Period))+geom_boxplot()+ylab("Invasion Risk")+xlab("GCMs")
+ggsave("InvasionRisk_Plot.png", path="Figures/", device = "png",width = 6, height = 4.5 )
